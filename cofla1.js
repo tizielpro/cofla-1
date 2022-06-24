d8c0344731b54994c49460efe3f1dec79b593799
Tiziano Montenegro <91438170+tizielpro@users.noreply.github.com>
2022-06-24 19:58:16 -0300

    primera subida
    sistema para vender helados

---
+++ cofla1.js
@@ -0,0 +1,91 @@
+let dineroCofla = prompt("cuanto dinero tienes Cofla?");
+let dineroRoberto = prompt("cuanto dinero tienes Roberto?");
+let dineroPedro = prompt("cuanto dinero tienes Pedro?");
+
+dineroCofla = dineroCofla;
+dineroRoberto = dineroRoberto;
+dineroPedro = dineroPedro;
+
+if(dineroCofla >= 0.6 && dineroCofla < 1){
+    alert("Cofla, te alcanza para un helado de agua");
+    alert(`y te sobran ${dineroCofla - 0.6}`)
+}
+else if(dineroCofla >= 1 && dineroCofla < 1.6){
+    alert("Cofla, te alcanza para un helado de crema");
+    alert(`y te sobran ${dineroCofla - 1}`)
+}
+else if(dineroCofla >= 1.6 && dineroCofla < 1.7){
+    alert("Cofla, te alcanza para un helado de marca heladix");
+    alert(`y te sobran ${dineroCofla - 1.6}`)
+}
+else if(dineroCofla >= 1.7 && dineroCofla < 1.8){
+    alert("Cofla, te alcanza para un helado de marca heladovich");
+    alert(`y te sobran ${dineroCofla - 1.7}`)
+}
+else if(dineroCofla >= 1.8 && dineroCofla < 2.9){
+    alert("Cofla, te alcanza para un helado de marca helardo");
+    alert(`y te sobran ${dineroCofla - 1.8}`)
+}
+else if(dineroCofla >= 2.9){
+    alert("Cofla, te alcanza para un potecito con helado con confites y para un pote de 1/4 kg");
+    alert(`y te sobran ${dineroCofla - 2.9}`)
+}
+else{
+    alert("Cofla, no te alcanza para ningun helado");
+}
+
+if(dineroRoberto >= 0.6 && dineroRoberto < 1){
+    alert("Roberto, te alcanza para un helado de agua");
+    alert(`y te sobran ${dineroCofla - 0.6}`)
+}
+else if(dineroRoberto >= 1 && dineroRoberto < 1.6){
+    alert("Roberto, te alcanza para un helado de crema");
+    alert(`y te sobran ${dineroCofla - 1}`)
+}
+else if(dineroRoberto >= 1.6 && dineroRoberto < 1.7){
+    alert("Roberto, te alcanza para un helado de marca heladix");
+    alert(`y te sobran ${dineroCofla - 1.6}`)
+}
+else if(dineroRoberto >= 1.7 && dineroRoberto < 1.8){
+    alert("Roberto, te alcanza para un helado de marca heladovich");
+    alert(`y te sobran ${dineroCofla - 1.7}`)
+}
+else if(dineroRoberto >= 1.8 && dineroRoberto < 2.9){
+    alert("Roberto, te alcanza para un helado de marca helardo");
+    alert(`y te sobran ${dineroCofla - 1.8}`)
+}
+else if(dineroRoberto >= 2.9){
+    alert("Roberto, te alcanza para un potecito con helado con confites y para un pote de 1/4 kg");
+    alert(`y te sobran ${dineroCofla - 2.9}`)
+}
+else{
+    alert("Roberto, no te alcanza para ningun helado");
+}
+
+if(dineroPedro >= 0.6 && dineroPedro < 1){
+    alert("Pedro, te alcanza para un helado de agua");
+    alert(`y te sobran ${dineroCofla - 0.6}`)
+}
+else if(dineroPedro >= 1 && dineroPedro < 1.6){
+    alert("Pedro, te alcanza para un helado de crema");
+    alert(`y te sobran ${dineroCofla - 1}`)
+}
+else if(dineroPedro >= 1.6 && dineroPedro < 1.7){
+    alert("Pedro, te alcanza para un helado de marca heladix");
+    alert(`y te sobran ${dineroCofla - 1.6}`)
+}
+else if(dineroPedro >= 1.7 && dineroPedro < 1.8){
+    alert("Pedro, te alcanza para un helado de marca heladovich");
+    alert(`y te sobran ${dineroCofla - 1.7}`)
+}
+else if(dineroPedro >= 1.8 && dineroPedro < 2.9){
+    alert("Pedro, te alcanza para un helado de marca helardo");
+    alert(`y te sobran ${dineroCofla - 1.8}`)
+}
+else if(dineroPedro >= 2.9){
+    alert("Pedro, te alcanza para un potecito con helado con confites y para un pote de 1/4 kg");
+    alert(`y te sobran ${dineroCofla - 2.9}`)
+}
+else{
+    alert("Pedro, no te alcanza para ningun helado");
+}
